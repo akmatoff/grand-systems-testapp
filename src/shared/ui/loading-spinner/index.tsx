50ffb17634b9e6133157509b063c50a90c33e409
akmatoff <akmatoff.exe@gmail.com>
2025-01-20 02:29:23 +0600

    add loading

---
+++ src/shared/ui/loading-spinner/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./loading-spinner.module.scss";
+
+export default function LoadingSpinner() {
+  return <div className={styles["spinner"]}></div>;
+}
--- src/widgets/products-list/index.tsx
+++ src/widgets/products-list/index.tsx
@@ -3,6 +3,7 @@ import ProductCard from "@/widgets/product-card/product-card";
 
 import styles from "./products-list.module.scss";
 import { useAppSelector } from "@/app/hooks";
+import LoadingSpinner from "@/shared/ui/loading-spinner";
 
 interface Props {
   products: IProduct[];
@@ -18,6 +19,19 @@ export default function ProductsList({ products, isLoading }: Props) {
 
   return (
     <>
+      {isLoading && (
+        <div
+          style={{
+            width: "100%",
+            height: "200px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <LoadingSpinner />
+        </div>
+      )}
       {!products.length && !isLoading && (
         <div className={styles["placeholder"]}>Продукты не найдены.</div>
       )}
